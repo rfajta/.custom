23cb69b152d916c578a610ef617b885cde243fd3
Robert Fajta <robert.fajta@prezi.com>
2013-12-22 13:12:31 +0100

    exporting color variables

--- initcolors.sh
+++ initcolors.sh
@@ -1,32 +1,32 @@
 # The values of these constants contain escape sequences
 # use Ctrl-q Ctrl-[ for ^[ and Ctrl-q Ctrl-] for ^]
 
-NO_COLOR="[0m"
+export NO_COLOR="[0m"
 
-FG_BLACK="[0;30m"
-FG_RED="[0;31m"
-FG_GREEN="[0;32m"
-FG_BROWN="[0;33m"
-FG_BLUE="[0;34m"
-FG_PURPLE="[0;35m"
-FG_CYAN="[0;36m"
-FG_LIGHT_GREY="[0;37m"
-FG_GREY="[1;30m"
-FG_LIGHT_RED="[1;31m"
-FG_LIGHT_GREEN="[1;32m"
-FG_YELLOW="[1;33m"
-FG_LIGHT_BLUE="[1;34m"
-FG_LIGHT_PURPLE="[1;35m"
-FG_LIGHT_CYAN="[1;36m"
-FG_WHITE="[1;37m"
+export FG_BLACK="[0;30m"
+export FG_RED="[0;31m"
+export FG_GREEN="[0;32m"
+export FG_BROWN="[0;33m"
+export FG_BLUE="[0;34m"
+export FG_PURPLE="[0;35m"
+export FG_CYAN="[0;36m"
+export FG_LIGHT_GREY="[0;37m"
+export FG_GREY="[1;30m"
+export FG_LIGHT_RED="[1;31m"
+export FG_LIGHT_GREEN="[1;32m"
+export FG_YELLOW="[1;33m"
+export FG_LIGHT_BLUE="[1;34m"
+export FG_LIGHT_PURPLE="[1;35m"
+export FG_LIGHT_CYAN="[1;36m"
+export FG_WHITE="[1;37m"
 
-BG_BLACK="[40m"
-BG_RED="[41m"
-BG_GREEN="[42m"
-BG_BROWN="[43m"
-BG_BLUE="[44m"
-BG_PURPLE="[45m"
-BG_CYAN="[46m"
-BG_LIGHT_GREY="[47m"
+export BG_BLACK="[40m"
+export BG_RED="[41m"
+export BG_GREEN="[42m"
+export BG_BROWN="[43m"
+export BG_BLUE="[44m"
+export BG_PURPLE="[45m"
+export BG_CYAN="[46m"
+export BG_LIGHT_GREY="[47m"
 
 
