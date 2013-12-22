4c11b79232adc8875e60cfd56415c7a89ba03311
Robert Fajta <robert.fajta@prezi.com>
2013-12-22 23:44:10 +0100

    cleanup, added backgound color support

--- prompt.sh
+++ prompt.sh
@@ -5,10 +5,9 @@
 . ~/.custom/initcolors.sh
 
 printPrompt() {
-  EXITCODE=${?#0}
-  SPACE=" "
-  OPEN_SQ_BRAQCKET="["
-  CLOSE_SQ_BRACKET="]"
+  local EXITCODE=${?#0}
+  local OPEN_SQ_BRAQCKET="["
+  local CLOSE_SQ_BRACKET="]"
 
   # takes an associative array name and optionally a key name
   # returns with the value found for that key, or returns with the item with 'default' key
@@ -17,86 +16,85 @@ printPrompt() {
   # the associative array must contain an item with 'default' key which may have a value of 'KEY'
   getValue() {
     local key="$2"
-    if [[ -z "$key" ]]
+    local arrayName="$1"
+    if [[ -z "${key}" ]]
     then
       key="default"
     fi
-    local color=""
-    color="$(eval echo "\${$1[$key]}")"
+    local color="$(eval echo "\${$arrayName[${key}]}")"
     if [[ -z "$color" ]]
     then
-      color="$(eval echo "\${$1[default]}")"
-      if [[ "$color" == "KEY" ]]
+      color="$(eval echo "\${$arrayName[default]}")"
+      if [[ "${color}" == "KEY" ]]
       then
-        color="$key"
+        color="${key}"
       fi
     fi
-    echo -n "$color"
+    echo -n "${color}"
   }
 
+  # If text is empty then returns with empty text, otherwise colored text
   getPart() {
-    local COLOR="$1"
-    local TEXT="$2"
-    if [[ -z "$TEXT" ]]
+    local color="$1"
+    local text="$2"
+
+    if [[ -z "${text}" ]]
     then
       echo -n ""
     else
-      echo -n "$COLOR""$TEXT""$NO_COLOR"
+      echo -n "${color}${text}"
     fi
   }
 
-  # the <user>
-  COMMAND_USER="\$USER"
-  declare -A COLORS_USER=(
-    [robertfa]=$FG_GREEN
-    [jenkins]=$FG_PURPLE
-    [default]=$FG_CYAN
-  )
-  COLOR_USER=\$\(getValue\ COLORS_USER\ \$USER\)
+  # # specify prompt background color per user, not used currently
+  # declare -A PROMPT_BG_COLORS
+  # PROMPT_BG_COLORS=(
+  #   [robertfa]=""
+  #   [root]=${BG_RED}
+  #   [default]=""
+  # )
+  # COLOR_PROMPT_BG='$(getValue PROMPT_BG_COLORS ${USER})'
+  COLOR_PROMPT_BG=""
 
+  # the <user>
+  COMMAND_USER='${USER}'
+  declare -A COLORS_USER=(
+    [robertfa]=${FG_GREEN}
+    [jenkins]=${FG_PURPLE}
+    [default]=${FG_CYAN}
+  )
+  COLOR_USER='$(getValue COLORS_USER ${USER})'
 
   # the @ sign
-  COMMAND_AT="@"
-  declare -A COLORS_AT=(
-    [default]="$FG_BROWN"
-  )
-  COLOR_AT=\$\(getValue\ COLORS_AT\)
+  COMMAND_AT='@'
+  COLOR_AT='${FG_BROWN}'
 
   # host
   # specify color for for each <host> plus for the 'default'
   declare -A COLORS_HOST=(
-    [localhost]=$FG_CYAN
-    [default]=$FG_LIGHT_PURPLE
+    [localhost]=${FG_CYAN}
+    [default]=${FG_LIGHT_PURPLE}
   )
   # abbreviation of each <host> for the tab name of konsole, plus ['default']=KEY
-  declare -A HOSTS HOSTS=(
+  declare -A HOST_ABBREVIATION=(
     [localhost]=l
     [default]=KEY
   )
   # no surrounding single or double quotes here
-  COMMAND_HOST=\"\$\(getValue\ HOSTS\ \$HOSTNAME\)\"
-  COLOR_HOST=\$\(getValue\ COLORS_HOST\ \$HOSTNAME\)
+  COMMAND_HOST='$(getValue HOST_ABBREVIATION $HOSTNAME)'
+  COLOR_HOST='$(getValue COLORS_HOST $HOSTNAME)'
 
   # <path>
-  COMMAND_PWD=\"\${PWD}\"
-  declare -A COLORS_PWD=(
-    [default]="$FG_BROWN"
-  )
-  COLOR_PWD=\$\(getValue\ COLORS_PWD\)
+  COMMAND_PWD='${PWD}'
+  COLOR_PWD='$FG_BROWN'
 
   # <time>
-  COMMAND_TIME=\$\(date\ +%H:%M:%S\)
-  declare -A COLORS_TIME=(
-    [default]="$FG_BROWN"
-  )
-  COLOR_TIME=\"\$\(getValue\ COLORS_TIME\)\"
+  COMMAND_TIME='$(date +%H:%M:%S)'
+  COLOR_TIME='$FG_BROWN'
 
   # <exit code>
-  COMMAND_EXITCODE=\"\${SPACE}\${EXITCODE}\${SPACE}\"
-  declare -A COLORS_EXITCODE=(
-    [default]="$BG_BLUE$FG_YELLOW"
-  )
-  COLOR_EXITCODE=\"\$\(getValue\ COLORS_EXITCODE\)\"
+  COMMAND_EXITCODE='\ ${EXITCODE}\ '
+  COLOR_EXITCODE='$BG_BLUE$FG_YELLOW'
 
   # <git>
   COMMAND_GIT=\$\(/usr/local/bin/vcprompt\ -f\ \'[\$FG_GREEN%a%m%u\ %b$FG_RED\%a\%m\%u\$FG_BROWN]\'\)
@@ -104,21 +102,10 @@ printPrompt() {
     [default]="$FG_BROWN"
   )
   COLOR_GIT=\"\$\(getValue\ COLORS_GIT\)\"
-
-
-  ##########
-
-  # color for <exit_code_of_last_comment_if_not_zero>
-  EXIT_CODE_COLOR="$BG_BLUE$FG_YELLOW"
-  EXEC_TIME_COLOR="$FG_BROWN"
-
-  # specify prompt background color
-  declare -A PROMPT_BG_COLORS
-  PROMPT_BG_COLORS=(
-    [user1]=""
-    [user2]=$BG_RED
-    [default]=""
-  )
+  COLOR_GIT_BRACKETS="$FG_BROWN"
+  COLOR_GIT_BRANCH="$FG_CYAN"
+  COLOR_GIT_CHANGES="$FG_RED"
+  COLOR_GIT_STASH="$FG_RED"
 
   getLengthOfVisiblePart() {
     local text="$1"
@@ -182,66 +169,66 @@ printPrompt() {
     local gitDir="$(getGitDir)"
     if [[ -n "$gitDir" ]]
     then
-      local BRANCH=$(git branch | grep "^* ")
-      BRANCH="${BRANCH:2}"
-      echo -n "$SPACE${FG_BROWN}${OPEN_SQ_BRAQCKET}${FG_CYAN}$BRANCH"
+      local branch=$(git branch | grep "^* ")
+      branch="${branch:2}"
+      echo -n " ${COLOR_GIT_BRACKETS}${COLOR_PROMPT_BG}${OPEN_SQ_BRAQCKET}${COLOR_GIT_BRANCH}${COLOR_PROMPT_BG}$branch"
       # staged files
       git diff --quiet --cached --exit-code
       if [[ ${?#0} ]]
       then
-        local changeIndicator="$changeIndicator""+"
+        local changeIndicator="${changeIndicator}+"
       fi
       # modified files
       git diff --quiet --exit-code
       if [[ ${?#0} ]]
       then
-        changeIndicator="$changeIndicator""*"
+        changeIndicator="${changeIndicator}*"
       fi
       # untracked files
       if [[ -n $(git ls-files "$gitDir" --other --exclude-standard) ]]
       then
-       changeIndicator="$changeIndicator""?"
+       changeIndicator="${changeIndicator}?"
       fi
      # unpushed commits
-      if [[ -n $(git log $BRANCH --not --remotes --oneline) ]]
+      if [[ -n $(git log $branch --not --remotes --oneline) ]]
       then
-       changeIndicator="$changeIndicator""!"
+       changeIndicator="${changeIndicator}!"
       fi
       # stashed stack depth
       local stashStackDepth="$(git stash list | wc -l | cut -f8 -d' ' | grep -v '^ 0$')"
       if [[ $changeIndicator ]]
       then
-        echo -n "${FG_RED}$SPACE$changeIndicator"
+        getPart "${COLOR_GIT_CHANGES}${COLOR_PROMPT_BG}" " $changeIndicator"
       fi
       if [[ "$stashStackDepth" != "0" ]]
       then
-        echo -n "${FG_RED}$SPACE$stashStackDepth"
+        getPart "${COLOR_GIT_STASH}${COLOR_PROMPT_BG}" " $stashStackDepth"
       fi
-      echo -n "${FG_BROWN}${CLOSE_SQ_BRACKET}"
+      echo -n "${COLOR_GIT_BRACKETS}${COLOR_PROMPT_BG}${CLOSE_SQ_BRACKET}"
     else
       echo -n ""
     fi
   }
 
   composeBeginning() {
-    eval "getPart "$COLOR_USER" "$COMMAND_USER""
-    eval "getPart "$COLOR_AT" "$COMMAND_AT""
-    eval "getPart "$COLOR_HOST" "$COMMAND_HOST""
-    echo -n "$SPACE"
-    eval "getPart "$COLOR_PWD" "$COMMAND_PWD""
+    eval "getPart "$COLOR_USER${COLOR_PROMPT_BG}" "$COMMAND_USER""
+    eval "getPart "$COLOR_AT${COLOR_PROMPT_BG}" "$COMMAND_AT""
+    eval "getPart "$COLOR_HOST${COLOR_PROMPT_BG}" "$COMMAND_HOST""
+    echo -n " "
+    eval "getPart "$COLOR_PWD${COLOR_PROMPT_BG}" "$COMMAND_PWD""
   }
 
   composeEnd() {
     composeGitPart
-    if [[ $EXITCODE ]]
+    if [[ ${EXITCODE} ]]
     then
-      echo -n "$SPACE"
+      echo -n " "
       eval "getPart "$COLOR_EXITCODE" "$COMMAND_EXITCODE""
-      echo -n "$SPACE"
+      echo -n "${NO_COLOR}${COLOR_PROMPT_BG} "
     else
-      echo -n "$SPACE"
+      echo -n " "
     fi
-    eval "getPart "$COLOR_TIME" "$COMMAND_TIME""
+    eval "getPart "${COLOR_TIME}${COLOR_PROMPT_BG}" "$COMMAND_TIME""
   }
 
   ###########################################
@@ -249,12 +236,13 @@ printPrompt() {
   ###########################################
 
   # startTime=$(timer)
+  echo -n "$COLOR_PROMPT_BG"
   local beginnig="$(composeBeginning)"
   local end="$(composeEnd)"
 
   echo -n "$beginnig"
   composeIndentation "$beginnig" "$end"
-  echo "$end"
+  echo "${end}${NO_COLOR}"
   echo "> "
   # log 1: $(timer $startTime)
 }
