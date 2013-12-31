1f38983f71d42e6dc5331b59c2fadbe9c83ae6f0
Robert Fajta <robert.fajta@prezi.com>
2013-12-31 17:22:20 +0100

    fixing typo

--- prompt.sh
+++ prompt.sh
@@ -174,7 +174,7 @@ printPrompt() {
       if [[ "$branch" == "(no branch)" ]]
       then
         branch=$(git log -1 --oneline --abbrev=5)
-        branch="<${branch:0:5}>"
+        branch="${branch:0:5}"
       fi
       echo -n " ${COLOR_GIT_BRACKETS}${COLOR_PROMPT_BG}${OPEN_SQ_BRAQCKET}${COLOR_GIT_BRANCH}${COLOR_PROMPT_BG}${branch}"
 
