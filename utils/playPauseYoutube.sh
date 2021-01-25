5c50c4f88644c3307c59b1aa3aed897be48eeef7
Robert Fajta <robert@digitalasset.com>
2021-01-25 16:55:04 +0100

    change

--- utils/playPauseYoutube.sh
+++ utils/playPauseYoutube.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+if [[ "$1" == "0" ]]
+then
+  keypress="0"
+else
+  keypress="space"
+fi
+
 if xdotool search --all --class --name 'youtube'
 then 
   # it is running, send a `space` keystroke to play / pause
-  xdotool search --all --class --name 'youtube' key space
+  xdotool search --all --class --name 'youtube' key $keypress
 else
   # it is not running, start it
   /usr/bin/firefox --class=youtube -P App --no-remote
